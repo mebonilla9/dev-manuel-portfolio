32507d0b7f7d774c7aabd40841c6c0c89ca16738
Manuel Ernesto Bonilla Muñoz <mebonilla9@gmail.com>
2023-04-03 09:27:25 -0500

    Added behavior to mobile link Navbar menu

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -15,11 +15,14 @@ const clickHome = () => {
   active.value = ''
   window.scrollTo(0, 0)
 }
-const setToggle = () => {
-  toggle.value = !toggle.value
-}
+/*const setToggle = (value) => {
+  toggle.value = value
+}*/
 const activeLink = computed(() => (title) => {
   return (active.value === title ? 'text-white' : 'text-secondary') + ' hover:text-white text-[18px] font-medium cursor-pointer'
+})
+const activeLinkMenu = computed(() => (title) => {
+  return (active.value === title ? 'text-white' : 'text-secondary') + ' font-poppins font-medium cursor-pointer text-[16px]'
 })
 const toggleMenu = computed(() => {
   return (!toggle.value ? 'hidden' : 'flex') + ' p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl'
@@ -34,16 +37,16 @@ const toggleMenu = computed(() => {
             Software Engineer</span></p>
       </RouterLink>
       <ul class="list-none hidden sm:flex flex-row gap-10">
-        <li v-for="link in navLinks" :key="link.id" :class="activeLink(link.title)">
+        <li v-for="link in navLinks" :key="link.id" :class="activeLink(link.title)" @click="active=link.title">
           <a :href="link.id">{{ link.title }}</a>
         </li>
       </ul>
       <div class="sm:hidden flex flex-1 justify-end items-center">
         <img :src="toggle ? close : menu" alt="menu" class="w-[28px] h-[28px] object-contain cursor-pointer"
-          @click="setToggle">
+          @click="toggle=!toggle">
         <div :class="toggleMenu">
           <ul class="list-none flex justify-end items-start flex-col gap-4">
-            <li v-for="link in navLinks" :key="link.id" :class="activeLink(link.title)">
+            <li v-for="link in navLinks" :key="link.id" :class="activeLinkMenu(link.title)" @click="">
               <a :href="link.id">{{ link.title }}</a>
             </li>
           </ul>
